2c62327db64dd0de4f8b91309126cfee62a42eb5
Pascal Surget <6539039+psurget@users.noreply.github.com>
2022-01-02 15:45:27 -0500

    Adaptation pour Filemaker

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@
 
 //The first function. Remove this.
 const btn = document.querySelector("button");
-btn.onclick = function () {
-  alert("You ran some JavaScript");
+btn.onclick = function() {
+    document.getElementById("result").innerHTML = "Réaction";
 };
-
-//
